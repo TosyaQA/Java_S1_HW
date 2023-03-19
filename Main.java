7161a0185eb770c521f5d399ce6999b63e17ddc7
Uliana Udalova <uljanaud@gmail.com>
2023-03-19 19:15:28 +0300

    Seminar_1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Seminar_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Task1 h1 = new Task1();
+        h1.numbers();
+
+        Task2 h2 = new Task2();
+        h2.simpleNumbers();
+
+        Task3 h3 = new Task3();
+        h3.calculator();
+
+    } 
+}
---
+++ Task1.java
@@ -0,0 +1,28 @@
+package Seminar_1;
+
+import java.util.Scanner;
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! (произведение чисел от 1 до n)
+
+public class Task1 {
+    
+    public void numbers(){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter number n: ");
+        int n = reader.nextInt();
+        reader.close();
+
+        int summ = 0;
+        for (int i = 0; i <= n; i++) {
+            summ = summ + i;
+        }
+
+        int productOf = 1;
+        for (int i = 1; i <= n; i++) {
+            productOf = productOf * i;
+        }
+
+        System.out.println("Cумма чисел: " + summ + '\n' + "Произведение чисел: " + productOf + '\n');
+
+    }
+}
